a9e7c50e6d41b8568689c5e06838508ede0e09e5
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-06-28 17:25:49 +0200

    add comments to MomentSerializationOptions

--- src/lib/moment-serialization-options.ts
+++ src/lib/moment-serialization-options.ts
@@ -1,8 +1,34 @@
 import {JsonType} from "./json-type";
 
 export interface MomentSerializationOptions {
+
+    /**
+     * This value will be returned, if the moment value is undefined, which should have been serialized,
+     * It must be a JSON compatible value.
+     * This means, it must be of one type of: string | boolean | number | object | [].
+     * If this is not set, this moment value will be skipped in serialization.
+     *
+     * Applies only to serialization.
+     */
     valueIfUndefined?: JsonType;
+
+    /**
+     * Uses moment.utc() instead of moment(), which parses the variable as utc
+     * and does not convert the time into local time of the running node instance.
+     *
+     * Applies to serialization & deserialization
+     */
     useUtc?: boolean;
+
+    /**
+     * String format which is used to serialize moment objects into, like 'YYYY-MM-DDTHH:mm:ss.SSSZZ'
+     *
+     * ISO-Value: complete RFC-3339 format, which is equal to ISO-8601, for example: 2009-01-01T12:00:00+01:00
+     *            would be written as YYYY-MM-DDTHH:mm:ss.SSSZZ. In contrast to moment js default,
+     *            this implementation keeps the offset in serialization.
+     *            This breaks the compatibility with javascript Date.toIsoString(),
+     *            but if it's not needed, this is the more complete option.
+     */
     serializationFormat?: string | 'ISO';
 }
 
