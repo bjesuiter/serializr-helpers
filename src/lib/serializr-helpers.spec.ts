cf4710f0a7b0798f7c3b583fc303c6a840d16f84
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-01 09:04:28 +0200

    add more tests for serialize helper

--- src/lib/serializr-helpers.spec.ts
+++ src/lib/serializr-helpers.spec.ts
@@ -1,5 +1,12 @@
 import {SerializrHelperModel} from "../test-utils/serializr-helper-model";
-import {deserializeFromJson, serializeToJson} from "./serializr-helpers";
+import {
+    deserializeFromJson,
+    deserializeFromPojo,
+    deserializeJson,
+    serializeJson,
+    serializeToJson,
+    serializeToPojo
+} from "./serializr-helpers";
 import {deserialize, serialize} from "serializr";
 
 describe('Serializr Helpers', () => {
@@ -18,6 +25,45 @@ describe('Serializr Helpers', () => {
         const helperJson = serializeToJson(helperModel);
         const targetModel = deserializeFromJson(SerializrHelperModel, helperJson);
         const validateModel = deserialize(SerializrHelperModel, JSON.parse(helperJson));
+
+        expect(targetModel).toEqual(validateModel);
+        expect(targetModel).toBeInstanceOf(SerializrHelperModel);
+        expect(validateModel).toBeInstanceOf(SerializrHelperModel);
+    });
+
+    it('serialize JSON alias for serializeToJson', () => {
+        const helper = new SerializrHelperModel();
+        const targetJson = serializeJson(helper);
+        const validateJson = JSON.stringify(serialize(helper));
+
+        expect(targetJson).toMatchSnapshot();
+        expect(targetJson).toEqual(validateJson);
+    });
+
+    it('deserialize JSON alias for deserializeFromJson', () => {
+        const helperModel = new SerializrHelperModel();
+        const helperJson = serializeJson(helperModel);
+        const targetModel = deserializeJson(SerializrHelperModel, helperJson);
+        const validateModel = deserialize(SerializrHelperModel, JSON.parse(helperJson));
+
+        expect(targetModel).toEqual(validateModel);
+        expect(targetModel).toBeInstanceOf(SerializrHelperModel);
+        expect(validateModel).toBeInstanceOf(SerializrHelperModel);
+    });
+
+    it('serialize To Pojo', () => {
+        const helperModel = new SerializrHelperModel();
+        const targetPojo = serializeToPojo(helperModel);
+        const validatePojo = serialize(helperModel);
+
+        expect(targetPojo).toEqual(validatePojo);
+    });
+
+    it('deserialize from Pojo', () => {
+        const helperModel = new SerializrHelperModel();
+        const helperPojo = serialize(helperModel);
+        const targetModel = deserializeFromPojo(SerializrHelperModel, helperPojo);
+        const validateModel = deserialize(SerializrHelperModel, helperPojo);
 
         expect(targetModel).toEqual(validateModel);
         expect(targetModel).toBeInstanceOf(SerializrHelperModel);
