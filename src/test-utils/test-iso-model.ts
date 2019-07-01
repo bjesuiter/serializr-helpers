9835e3baa86d54c75bd19880fa76a5720a4913b9
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-01 09:23:47 +0200

    adjust TestIsoModel to allow undefined time values for testing

--- src/test-utils/test-iso-model.ts
+++ src/test-utils/test-iso-model.ts
@@ -5,9 +5,9 @@ import {MomentSerializationSchema} from "../lib/moment-serialization-schema";
 export class TestIsoModel {
 
     @serializable(MomentSerializationSchema())
-    public testMoment: Moment;
+    public testMoment?: Moment;
 
-    constructor(testMoment: Moment) {
+    constructor(testMoment?: Moment) {
         this.testMoment = testMoment;
     }
 }
