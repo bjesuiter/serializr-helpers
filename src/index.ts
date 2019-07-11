264e934724a71edd43655762a81e5ff4f885cc3c
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-11 21:33:32 +0200

    add json-types to exported things in index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ export * from "./lib/moment-serialization-schema"
 export * from "./lib/moment-serialization-options"
 
 /**
+ * General JSON Type for Typescript
+ */
+export * from "./lib/json-types"
+
+/**
  * Tools / Helpers
  */
 export * from "./lib/serializr-helpers"
